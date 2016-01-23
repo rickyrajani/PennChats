183a8de2e48375b54c196f86f4a42e89a277879b
sahilahuja1 <sahilahu@seas.upenn.edu>
2016-01-23 00:27:24 -0500

    file upload structure

---
+++ upload_file/doc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			body { margin:0; padding:0; background-color: #CCC}
+			.fileuploadholder {
+				width: 400px;
+				height: 200px;
+				margin: 60px auto 0px auto;
+				background-color: #FFF;
+				border: 1px solid #000
+			}
+		</style>
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+		<title>File Upload</title>
+	</head>
+
+	<body>
+		<div class="fileuploadholder">
+
+		</div>
+	</body>
+</html>
