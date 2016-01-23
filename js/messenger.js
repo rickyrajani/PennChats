78f2307eb37f0fb1105adcd2ddf8a40e5a1e0cb5
Ricky Rajani <rickyrajani@seas1018.wireless-pennnet.upenn.edu>
2016-01-22 22:57:56 -0500

    pubnub code added

---
+++ js/messenger.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  // Initialize the PubNub API connection.
+  var pubnub = PUBNUB.init({
+    publish_key: 'pub-c-c202562b-e396-4abe-aa0d-3f2f120bb2d0',
+    subscribe_key: 'sub-c-2f8618a4-c17b-11e5-b684-02ee2ddab7fe'
+  });
+ 
+  // Grab references for all of our elements.
+  var messageContent = $('#messageContent'),
+      sendMessageButton = $('#sendMessageButton'),
+      messageList = $('#messageList');
+ 
+  // Handles all the messages coming in from pubnub.subscribe.
+  function handleMessage(message) {
+    var messageEl = $("<li class='message'>"
+        + "<span class='username'>" + message.username + ": </span>"
+        + message.text
+        + "</li>");
+    messageList.append(messageEl);
+    messageList.listview('refresh');
+ 
+    // Scroll to bottom of page
+    $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 'slow');
+  };
+ 
+  // Compose and send a message when the user clicks our send message button.
+  sendMessageButton.click(function (event) {
+    var message = messageContent.val();
+ 
+    if (message != '') {
+      pubnub.publish({
+        channel: 'chat',
+        message: {
+          username: 'test',
+          text: message
+        }
+      });
+ 
+      messageContent.val("");
+    }
+  });
+ 
+  // Also send a message when the user hits the enter button in the text area.
+  messageContent.bind('keydown', function (event) {
+    if((event.keyCode || event.charCode) !== 13) return true;
+    sendMessageButton.click();
+    return false;
+  });
+ 
+  // Subscribe to messages coming in from the channel.
+  pubnub.subscribe({
+    channel: 'chat',
+    message: handleMessage
+  });
+});
