fa09fc07ba01f18ff015ab73ad93a31405488d3c
sahilahuja1 <sahilahu@seas.upenn.edu>
2016-01-23 21:28:46 -0500

    fixed small bug

--- uploadscript.js
+++ uploadscript.js
@@ -44,7 +44,7 @@ function readBlob(opt_startByte, opt_stopByte) {
 				courseRoom.set("end_date", enddate[1]);
 		    	courseRoom.set("day_of_week", sessionday[1]);
 				
-				currentUser.add("rooms", courseRoom);
+				currentUser.add("rooms", courseRoom.objectId);
 
 				courseRoom.save();
 			}
